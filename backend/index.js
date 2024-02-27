8c5c7ea9afd683b4b5a27110728a340fda67b726
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-02-27 21:55:12 +0530

    added mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
-import express from "express";
-import { PORT } from "./config.js"
+import express, { response } from "express";
+import { PORT } from "./config.js";
+import mongoose from 'mongoose';
 
 const app = express();
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send(`Welcome to Book Shop`)
+});
 
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
