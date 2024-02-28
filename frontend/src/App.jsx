cb07005625f04429fa9009aab00cfc931e053e1e
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-02-28 11:04:29 +0530

    added page routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import ShowBook from './pages/ShowBook'
+import EditBooks from './pages/EditBooks'
+import DeleteBooks from './pages/DeleteBooks'
+import CreateBooks from './pages/CreateBooks'
 
 const App = () => {
   return (
     <Routes>
-      <Route path='' element={} />
-      <Route path='' element={} />
-      <Route path='' element={} />
-      <Route path='' element={} />
-      <Route path='' element={} />
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBooks />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBooks />} />
+      <Route path='/books/delete/:id' element={<DeleteBooks />} />
     </Routes>
   )
 }
