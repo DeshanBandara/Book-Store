2435f656a0729483c217a5c3ae46ae9ed22dc33d
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-02-28 10:46:23 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+    </Routes>
   )
 }
 
